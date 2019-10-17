8efd746b0c8cb475ecf626e17a8bcfb248227154
SHEY LOUIS CHIA <shloch2007@yahoo.fr>
2019-10-17 12:23:51 +0100

    removing unnecessary data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import logoCalc from './logoCalc.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logoCalc} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          REACT CALCULATOR
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
