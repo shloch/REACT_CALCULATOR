fe776231c1fa039c01e5c4bc8fb2a26a7ae35004
SHEY LOUIS CHIA <shloch2007@yahoo.fr>
2019-10-18 14:41:00 +0100

    DISPLAY component filename typo

---
+++ src/components/Display.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Display extends Component {
+
+  render() {
+    return (
+      <div id="display-component">
+        {this.props.calculationResult}
+      </div>
+    )
+  }
+}
+
+Display.propTypes = {
+  calculationResult: PropTypes.string
+
+};
+Display.defaultProps = {
+  calculationResult: '0'
+};
+
+export default Display
