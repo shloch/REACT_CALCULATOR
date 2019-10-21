e4063923a91703b58e9c278c31f675fe256fd877
SHEY LOUIS CHIA <shloch2007@yahoo.fr>
2019-10-21 12:58:13 +0100

    implementing calculator logic

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,19 +1,40 @@
-var Calculator = (function () {
+import operate from './operate';
 
-  let mod = {};
-  mod.calculate = function (calcObj, button) {
-    let { total, next, operation } = calcObj;
-    if (button === '+/1') {
-      total *= -1;
-      next *= -1;
-    }
-    return { total, next, operation }
+const calculate = (obj, buttonName) => {
+  let { total, next, operation } = obj;
+  let operations = ['+', '-', 'x', '÷', '%'];
+  let numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
+  if (buttonName === '+/-') {
+    if (total !== null) total *= -1;
+    if (next !== null) next *= -1;
   }
 
-  return mod;
-})();
+  if (numbers.includes(buttonName)) {
+    if (total === null)
+      total = +buttonName;
+    else
+      next = +buttonName;
+    total = operate(total, next, operation);
+  }
 
-export {
-  Calculator,
-};
+  if (operations.includes(buttonName)) {
+    if (total !== null && next !== null)
+      operation = buttonName;
+    else
+      return { total, next, operation };
+  }
+
+  if (buttonName === '=') {
+    if (total !== null && next !== null && operation !== null) {
+      total = operate(total, next, operation);
+    }
+    next = null;
+    operation = null;
+    return { total, next, operation };
+  }
+
+  return { total, next, operation };
+}
+
+export default calculate;
--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,20 +1,27 @@
-import 'big.js';
-var Operate = (function () {
+import Big from 'big.js';
 
-  let mod = {};
-  mod.operate = function (calcObj, button) {
-    let { total, next, operation } = calcObj;
-    if (button === '+/1') {
-      total *= -1;
-      next *= -1;
-    }
-    return { total, next, operation }
-
+export default function operate(numbernumber1, numbernumber2, operation) {
+  let total = '';
+  const number1 = Big(numbernumber1);
+  const number2 = Big(numbernumber2);
+  switch (operation) {
+    case '+':
+      total = number1.plus(number2).toString();
+      break;
+    case '-':
+      total = number1.minus(number2).toString();
+      break;
+    case 'x':
+      total = number1.times(number2).toString();
+      break;
+    case '/':
+      total = number1.div(number2).toString();
+      break;
+    case '%':
+      total = number1.mod(number2).toString();
+      break;
+    default:
+      alert(`operation '${operation}' not available `);
   }
-
-  return mod;
-})();
-
-export {
-  Operate,
-};
+  return total;
+}
