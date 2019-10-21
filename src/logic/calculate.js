0b9c49e3513222c23a36e9d781498fbb9d8b298e
SHEY LOUIS CHIA <shloch2007@yahoo.fr>
2019-10-21 09:18:40 +0100

    modifying stickler error

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -2,7 +2,7 @@ var Calculator = (function () {
 
   let mod = {};
   mod.calculate = function (calcObj, button) {
-    const { total, next, operation } = calcObj;
+    let { total, next, operation } = calcObj;
     if (button === '+/1') {
       total *= -1;
       next *= -1;
